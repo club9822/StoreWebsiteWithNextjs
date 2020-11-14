db6fbccc2ae4db1df1813fae56bc89bf2deeb2c2
daap app <club9822@gmail.com>
2020-11-14 16:49:30 +0330

    Added next-offline

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import {useEffect} from 'react';
 import '../styles/global.css';
 import '../styles/tailwind.css';
+//https://github.com/hanford/next-offline#runtime-registration
+import { register, unregister } from 'next-offline/runtime'
 
 export default function App({ Component, pageProps }) {
+  
+     /**  register service worker
+      * Default service worker path is '/service-worker.js' 
+      * Refer https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register for default scope rules
+      *
+    */
+  useEffect(()=>{
+    register()
+    return ()=>{
+      unregister()
+    }
+  },[])
+
   return <Component {...pageProps} />;
 }
